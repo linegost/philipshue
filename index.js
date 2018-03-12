e54456982de9c19133233c5ae6b2debd9c8a03b0
tanerolcxy <36379909+tanerolcxy@users.noreply.github.com>
2018-03-12 17:14:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ var five = require('johnny-five');
 var board = new five.Board();
 board.on('ready', function() {
 
-// var led = new five.Led.RGB([6, 5, 3]);
 var led = new five.Led(9);
 
 io.on('connection', function (socket) {
 socket.on('msg', function (data) {
-
         if(data == 'cmd') {
-        led.blink(500);
+                
+        led.blink(500); // --- EDIT HERE ---
+                
         }
-
     });
   });
 });
