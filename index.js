27bb4de43d2cbd848517ceafa506f8d810d39bba
tanerolcxy <36379909+tanerolcxy@users.noreply.github.com>
2018-03-12 16:33:37 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.get('/', function(req, res) { res.sendFile(__dirname + '/index.html'); });
+var five = require('johnny-five');
+var board = new five.Board();
+board.on('ready', function() {
+
+var led = new five.Led.RGB([6, 5, 3]);
+var led = new five.Led(9);
+
+io.on('connection', function (socket) {
+socket.on('msg', function (data) {
+
+        if(data == 'cmd') {
+
+        }
+
+    });
+  });
+});
+
+http.listen(4040, function() {});
