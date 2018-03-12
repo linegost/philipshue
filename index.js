fbcb4bf808b2e7bf923dc5bc4636ec0bef5addcf
tanerolcxy <36379909+tanerolcxy@users.noreply.github.com>
2018-03-12 23:06:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-app.get('/', function(req, res) { res.sendFile(__dirname + '/index.html'); });
+app.use(express.static(path.join(__dirname, '/')));
 var five = require('johnny-five');
 var board = new five.Board();
 board.on('ready', function() {
