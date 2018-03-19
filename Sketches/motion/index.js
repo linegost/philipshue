de232f5984e8363f059096e649327587d2c71838
Johan Hellgren <honajnahoj@gmail.com>
2018-03-19 12:17:24 +0100

    Yet more motion cleanup, better comments

--- Sketches/motion/index.js
+++ Sketches/motion/index.js
@@ -19,29 +19,18 @@ board.on('ready', function()
 
   let keycard = new five.Pin(10);
 
-
-  //Store brightness in variable because led.intensity doesn't appear to be readable
-  let LEDBrightness = 0;
-  let oldZ = 0;
   led.on();
   led.intensity(0);
 
+  //Set LED brightness based on phone z axis movement
   io.on('connection', function (socket)
   {
     socket.on('msg', function (data)
     {
-    //Increase brightness if motion data is larger than previously
-    if(LEDBrightness < data)
-    {
-      LEDBrightness = data;
-      led.intensity(100 - LEDBrightness);
-    }
-    // LEDBrightness = data;
-    console.log(LEDBrightness);
-    led.intensity(100 - LEDBrightness);
+      console.log(Math.floor(data));
+      led.intensity(100 - data);
     });
   });
 });
 
 http.listen(4040, function() {});
- 
