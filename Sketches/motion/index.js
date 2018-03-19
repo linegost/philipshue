caa140998bf55df4aca56c1e69c4cdf7149f53b0
Johan Hellgren <honajnahoj@gmail.com>
2018-03-19 02:04:06 +0100

    More motion cleanup

--- Sketches/motion/index.js
+++ Sketches/motion/index.js
@@ -6,35 +6,42 @@ var io = require('socket.io')(http);
 app.use(express.static(path.join(__dirname, '/')));
 var five = require('johnny-five');
 var board = new five.Board();
-board.on('ready', function() {
+board.on('ready', function()
+{
+  var led = new five.Led.RGB(
+  {
+    pins: {
+      red: 6,
+      green: 5,
+      blue: 3
+    }
+  });
 
-var led = new five.Led.RGB({
-  pins: {
-    red: 6,
-    green: 5,
-    blue: 3
-  }
-});
+  let keycard = new five.Pin(10);
 
-//Store brightness in variable because led.intensity doesn't appear to be readable
-let LEDBrightness = 0;
 
-led.on();
-led.intensity(0);
+  //Store brightness in variable because led.intensity doesn't appear to be readable
+  let LEDBrightness = 0;
+  let oldZ = 0;
+  led.on();
+  led.intensity(0);
 
-io.on('connection', function (socket) {
-  socket.on('msg', function (data) {
+  io.on('connection', function (socket)
+  {
+    socket.on('msg', function (data)
+    {
     //Increase brightness if motion data is larger than previously
     if(LEDBrightness < data)
     {
       LEDBrightness = data;
       led.intensity(100 - LEDBrightness);
     }
-    //led.intensity(100 - data);
-    console.log(Math.floor(data));
-      });
+    // LEDBrightness = data;
+    console.log(LEDBrightness);
+    led.intensity(100 - LEDBrightness);
     });
   });
+});
 
 http.listen(4040, function() {});
  
